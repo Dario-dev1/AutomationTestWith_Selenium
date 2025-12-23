525102e1a1c9868643ac5790118a1bb1acfe8f4d
Dario Avelino <avelinodarioricardo@gmail.com>
2025-12-23 13:04:41 +0100

    Adicionar arquivos de projeto.

---
+++ AutomationTestWith_Selenium/UnitTest1.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AutomationTestWith_Selenium
+{
+    public class Tests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [Test]
+        public void ExecuteTest()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com");
+            var searchBox = driver.FindElement(By.Name("q"));
+            searchBox.SendKeys("Teste Selenium");
+
+            Assert.That(driver.Title, Is.EqualTo("Google"));
+        }
+
+        [TearDown]
+        public void OneTimeTearDown()
+        {
+            driver?.Quit();
+            driver?.Dispose();
+        }
+    }
+}
