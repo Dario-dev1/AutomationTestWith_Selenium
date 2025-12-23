6d5554ffdebad73e957800b357949a9201fbd332
Dario Avelino <avelinodarioricardo@gmail.com>
2025-12-23 15:48:15 +0100

    updates

--- AutomationTestWith_Selenium/UnitTest1.cs
+++ AutomationTestWith_Selenium/UnitTest1.cs
@@ -6,29 +6,42 @@ namespace AutomationTestWith_Selenium
 {
     public class Tests
     {
+        //LOGIN IN AMAZON WEBSITE TEST
         private IWebDriver driver;
 
         [SetUp]
         public void Initialize()
         {
             driver = new ChromeDriver();
-            driver.Manage().Window.Maximize();
+            //Go to amazon page
+            driver.Navigate().GoToUrl("https://www.amazon.com");
         }
 
         [Test]
         public void ExecuteTest()
         {
-            driver.Navigate().GoToUrl("https://www.google.com");
-            var searchBox = driver.FindElement(By.Name("q"));
-            searchBox.SendKeys("Teste Selenium");
+            driver.Manage().Window.Maximize();
+            IWebElement Shopping = driver.FindElement(By.ClassName("a-button-text"));
+            Shopping.Click();
+            IWebElement SigniIn = driver.FindElement(By.Id("nav-link-accountList"));
+            SigniIn.Click();
 
-            Assert.That(driver.Title, Is.EqualTo("Google"));
+            IWebElement EmailField = driver.FindElement(By.Id("ap_email_login"));
+            EmailField.SendKeys("pewpew@abv.bg");
+
+            IWebElement ContinueButton = driver.FindElement(By.Id("continue"));
+            ContinueButton.Click();
+
+            IWebElement ErrorMessage = driver.FindElement(By.ClassName("a-alert-heading"));
+            String ActualErrorMessageTexte = ErrorMessage.Text;
+            String ExpectedErrorMessageText = "There was a problem";
+            Assert.Equals(ActualErrorMessageTexte, ExpectedErrorMessageText);
         }
 
         [TearDown]
         public void OneTimeTearDown()
         {
-            driver?.Quit();
+            //driver?.Quit();
             driver?.Dispose();
         }
     }
